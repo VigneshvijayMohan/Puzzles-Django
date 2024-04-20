f8b8c29e0dd9b99f505c6b8d2d54bce1a92d695b
Vignesh Mohan <so@yahoo.com>
2024-04-20 19:05:57 +0530

    Added Models

--- puzzles/quiz/admin.py
+++ puzzles/quiz/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Profile, Category, Player, Level, Question, ScoreBoard
 # Register your models here.
+
+
+admin.site.register(Profile)
+admin.site.register(Category)
+admin.site.register(Player)
+admin.site.register(Level)
+admin.site.register(Question)
+admin.site.register(ScoreBoard)
--- puzzles/quiz/models.py
+++ puzzles/quiz/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone =models.CharField(max_length=20, blank=True)
+    
+    
+    def __str__(self):
+        return self.user.username
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    points = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+class Level(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    levels = models.ManyToManyField(Level)
+    text = models.TextField()
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    correct_answer = models.CharField(max_length=1, choices=[('1', 'Option 1'), ('2', 'Option 2'), ('3', 'Option 3'), ('4', 'Option 4')])
+    # explanation = models.TextField(blank=True)  # Optional explanation for the correct answer
+    # image = models.ImageField(upload_to='question_images/', blank=True)  # Optional image for the question
+
+    # Additional fields for flexibility and customization
+    # question_type = models.CharField(max_length=20, default='Multiple Choice')  # Type of question
+    # difficulty_level = models.ForeignKey(Level, on_delete=models.SET_NULL, null=True, blank=True)  # Difficulty level
+
+    def __str__(self):
+        return self.text[:50]  # Display first 50 characters of the question text
+
+
+
+class ScoreBoard(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    
+    def get_score(self):
+        return self.player.points
+    
+    def __str__(self):
+        return f"{self.player.first_name}'s Score"
+
+
