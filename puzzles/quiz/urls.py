8ded146b71fc6a03158682348f9b527389816032
Vignesh Mohan <so@yahoo.com>
2024-04-25 10:50:56 +0530

    Added Login and Logout

--- puzzles/quiz/urls.py
+++ puzzles/quiz/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
     path('quiz/<str:foo>/', views.category, name='category'),
     path("page/", views.page, name="page"),
+    path("login/",views.login_user, name = "login" ),
+    path("logout/",views.logout_user, name = "logout" ),
+    
 ]
