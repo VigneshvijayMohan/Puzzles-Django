f2f0cdb12d75ec6211212ef0bab9cc6a0fe0cfc6
Vignesh Mohan <so@yahoo.com>
2024-04-26 09:29:08 +0530

    Quiz pages and logic added

--- puzzles/quiz/urls.py
+++ puzzles/quiz/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path('quiz/<str:foo>/', views.category, name='category'),
+    path('quiz/<str:foo>/<str:level>/', views.quiz, name='quiz'),
     path("page/", views.page, name="page"),
     path("login/",views.login_user, name = "login" ),
     path("logout/",views.logout_user, name = "logout" ),
