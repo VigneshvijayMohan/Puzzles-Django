4c07c78a769c87d321682ef9d9b97b8a974d8764
Vignesh Mohan <so@yahoo.com>
2024-04-27 19:27:04 +0530

    Score added

--- puzzles/quiz/urls.py
+++ puzzles/quiz/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("login/",views.login_user, name = "login" ),
     path("logout/",views.logout_user, name = "logout" ),
     path("register/",views.register_user, name = "register" ),
+    path("score/", views.score, name="score"),
     
 ]
