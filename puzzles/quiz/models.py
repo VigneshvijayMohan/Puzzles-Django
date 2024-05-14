4787508ef35d439f8b24470f45005c465cbd860c
Vignesh Mohan <so@yahoo.com>
2024-05-14 11:22:54 +0530

    Minor issues resolved

--- puzzles/quiz/models.py
+++ puzzles/quiz/models.py
@@ -19,6 +19,7 @@ class Description(models.Model):
     
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    description= models.CharField(max_length=200 , default = 0)
 
     def __str__(self):
         return self.name
